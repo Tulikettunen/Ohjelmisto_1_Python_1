55a71b095e2ce69875f11cdd51ad99fc52e1c4d7
Tulikettunen <tuuli.mrl@gmail.com>
2024-01-16 10:31:34 +0200

    Initial commit

---
+++ TestiKoodi1.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+Name=input("Enter your name: \n")
+CourseName= input("Which course you do: \n")
+
+def welcometext (Value1, value2):
+    print("welcome to " + value2 + ", " + Value1 +"!")
+
+welcometext(Name, CourseName)
+
+
