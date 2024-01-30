8183706e31526991002c563584e9562e62d96ffc
Tulikettunen <tuuli.mrl@gmail.com>
2024-01-30 11:35:02 +0200

    Merge remote-tracking branch 'main/master'

--- Moduuli_5/Moduuli_5_teht_1.py
+++ Moduuli_5/Moduuli_5_teht_1.py
@@ -4,3 +4,16 @@ Ohjelma heittää kerran kaikkia arpakuutioita ja tulostaa silmälukujen summan.
 Käytä for-toistorakennetta.
 """
 
+import random
+
+dice_amount = int(input("Syötä noppien lukumäärä: \n"))
+summa = 0
+
+
+for dice in range(0, dice_amount):
+    noppa = random.randint(1, 6)
+    print(noppa)  # trackää toimiiko ohjelma oikein
+    summa += noppa
+
+
+print(f"Noppien silmäluku on: {summa}")
