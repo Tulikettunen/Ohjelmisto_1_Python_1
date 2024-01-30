d8a7fd31e2172e20bfe78c1378d594dff211fef8
Tulikettunen <tuuli.mrl@gmail.com>
2024-01-30 19:29:46 +0200

    Merge remote-tracking branch 'main/master'

---
+++ Moduuli_5/Moduuli_5_teht_3.py
@@ -0,0 +1,24 @@
+"""
+Kirjoita ohjelma, joka kysyy käyttäjältä kokonaisluvun ja ilmoittaa, onko se alkuluku.
+Tässä tehtävässä alkulukuja ovat luvut, jotka ovat jaollisia vain ykkösellä ja itsellään.
+
+Esimerkiksi luku 13 on alkuluku, koska se voidaan jakaa vain luvuilla 1 ja 13 siten,
+että jako menee tasan.
+Toisaalta esimerkiksi luku 21 ei ole alkuluku, koska se voidaan jakaa tasan myös
+luvulla 3 tai luvulla 7.
+"""
+
+
+number = int(input("Enter a number: "))
+
+for i in range(2, round(number / 2)):
+    if number % i == 0:
+        print(f"Luku {number} ei ole alkuluku, se on jaollinen ainakin luvulla {i}")
+        break
+    else:
+        print(f"Luku {number} on alkuluku!")
+
+
+
+
+
