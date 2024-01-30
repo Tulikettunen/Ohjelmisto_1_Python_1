d98aa9ffd2873175fe54cebd7f5e32ba0b9271e0
Tulikettunen <tuuli.mrl@gmail.com>
2024-01-30 20:31:30 +0200

    Merge remote-tracking branch 'main/master'

---
+++ Moduuli_6/Moduuli_6_teht_2.py
@@ -0,0 +1,23 @@
+"""
+Muokkaa edellistä funktiota siten, että funktio saa parametrinaan nopan
+tahkojen yhteismäärän. Muokatun funktion avulla voit heitellä esimerkiksi
+21-tahkoista roolipelinoppaa. Edellisestä tehtävästä poiketen nopan
+heittelyä jatketaan pääohjelmassa kunnes saadaan nopan maksimisilmäluku,
+joka kysytään käyttäjältä ohjelman suorituksen alussa.
+"""
+
+
+import random
+silmaluku = int(input("Halutt nopan maximi silmäluku: "))
+noppanen = 0
+
+
+def noppa(x):
+    x = silmaluku
+    nopp = random.randint(1, x)
+    return nopp
+
+
+while noppanen < silmaluku:
+    noppanen = noppa(silmaluku)
+    print(noppanen)
