582297e76966a30007adeffdf3b4370294089911
Tulikettunen <tuuli.mrl@gmail.com>
2024-02-11 03:20:51 +0200

    Merge remote-tracking branch 'main/master'

--- Moduuli_6/Moduuli_7_teht_1.py
+++ Moduuli_6/Moduuli_7_teht_1.py
@@ -8,17 +8,26 @@ mittaiseksi siten, että joulukuu on ensimmäinen talvikuukausi.
 
 vuodenajat = ("Talvi", "Kevät", "Kesä", "Syksy")
 
-käyttäjän_kuukausi = int(input("Anna kuukauden numero: \n"))
+käyttäjän_kuukausi = 2
 
-while käyttäjän_kuukausi < 1 or käyttäjän_kuukausi > 12:
-    käyttäjän_kuukausi = int(input("Anna validi kuukauden numero: \n"))
+while type(käyttäjän_kuukausi) == str or käyttäjän_kuukausi >= 1 or käyttäjän_kuukausi <= 12:
+    käyttäjän_kuukausi = input("Anna kuukauden numero: \n")
 
-if käyttäjän_kuukausi in range(3, 6):
-    print(f"Vuodenaika on {vuodenajat[1]}")
-elif käyttäjän_kuukausi in range(6, 9):
-    print(f"Vuodenaika on {vuodenajat[2]}")
-elif käyttäjän_kuukausi in range(9, 12):
-    print(f"Vuodenaika on {vuodenajat[3]}")
-else:
-    print(f"Vuodenaika on {vuodenajat[0]}")
-
+    try:
+        käyttäjän_kuukausi = int(käyttäjän_kuukausi)
+        if käyttäjän_kuukausi in range(3, 6):
+            print(f"Vuodenaika on {vuodenajat[1]}")
+            break
+        elif käyttäjän_kuukausi in range(6, 9):
+            print(f"Vuodenaika on {vuodenajat[2]}")
+            break
+        elif käyttäjän_kuukausi in range(9, 12):
+            print(f"Vuodenaika on {vuodenajat[3]}")
+            break
+        elif käyttäjän_kuukausi == 12 or käyttäjän_kuukausi in range(1, 3):
+            print(f"Vuodenaika on {vuodenajat[0]}")
+            break
+        else:
+            print("Oot idiootti, vuodes on vaa 12 kuukautta :P ")
+    except Exception as e:
+        print("Oletpa typerä, kirjain ei ole kuun numero. Anna validi luku")
--- Moduuli_6/Moduuli_7_teht_3.py
+++ Moduuli_6/Moduuli_7_teht_3.py
@@ -12,6 +12,56 @@ helposti selaimen avulla.)
 """
 
 
+import mysql.connector
 
 
+connection = mysql.connector.connect(
+    host="127.0.0.1",
+    port=3306,
+    database="flight_game",
+    user="root",
+    password="root",
+    autocommit=True
+)
+set_lentokentät = set()
+dict_lentokentät = {}
+
+def kenttä_add(icao):
+    icao = input("Syötä lentokentän koodi: \n")
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT airport.name, airport.ident FROM airport WHERE airport.ident = '{icao}';")
+    if cursor.rowcount == 0:
+        print("Hakemaasi lentokenttää ei ole olemassa.")
+    elif cursor.rowcount == 1:
+        if cursor.fetchone() in dict_lentokentät:
+            print("Hakemasi lentokenttä on jo tallennettu.")
+        else:
+            dict_lentokentät[f"{icao}"] = cursor.fetchone()
+            print("Uusi lentokenttä lisätty kirjastoon")
+    else:
+        print("Useampi hakutulos, tarkenna hakua")
+    return
+
+def kenttä_haku(icao):
+    icao = input("Syötä lentokentän koodi: \n")
+    print(dict_lentokentät[icao])
+
+while True:
+    choice = input("Jos haluat hakea tietoa: "
+                   "valitse 1. \n Jos haluat lisätä uuden: valitse 2. "
+                   "\nJos haluat lopettaa: paina 3: \n")
+    if int(choice) == 1:
+        ICAO = input("Syötä kentän ICAO-koodi: \n")
+        print(f"Hakemasi kenttä: {kenttä_haku()}")
+    elif int(choice) == 2:
+        ICAO = input("Syötä kentän ICAO-koodi: \n")
+        kenttä_add(ICAO)
+    elif int(choice) == 3:
+        False
+
+    else:
+        choice = input("Syötäthän jonkun numeron 1-3!\n"
+                        "Jos haluat hakea tietoa: valitse 1. \n "
+                        "Jos haluat lisätä uuden: valitse 2. \n"
+                        "Jos haluat lopettaa: paina 3: \n")
 
